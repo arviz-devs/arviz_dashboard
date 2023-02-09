4c874f15b9513119ae87656c082071aa73e9155e
Yilin Xia <xia.yl@outlook.com>
2023-02-09 05:29:32 -0600

    create ppc_dashboard (#10)
    * create ppc_dashboard
    #7
    
    * resolve issues
    
    ---------
    
    Co-authored-by: Osvaldo A Martin <aloctavodia@gmail.com>

--- arviz_dashboard/__init__.py
+++ arviz_dashboard/__init__.py
@@ -1,5 +1,6 @@
 """Bayesian Dashboards built on top of Panel"""
 __version__ = "0.0.1a"
 
-from .elpd import dashboard_elpd
 from arviz_dashboard.marginal.one_d import posterior_marginal1d
+from .elpd import dashboard_elpd
+from .ppc import dashboard_ppc
