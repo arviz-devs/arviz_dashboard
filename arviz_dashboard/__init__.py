1e0a150d63bdd6527e869f31bee161570bbbfb4f
Andy Maloney <60523020+ndmlny-qs@users.noreply.github.com>
2023-04-11 10:32:32 -0500

    Add trace dashboard (#12)
    - Add a new dashboard that plots KDEs and traces.
    - Add a trace example notebook.
    - Updates the posterior marginal 1D example to use reproducible
      components defined in this PR.

--- arviz_dashboard/__init__.py
+++ arviz_dashboard/__init__.py
@@ -1,6 +1,7 @@
 """Bayesian Dashboards built on top of Panel"""
 __version__ = "0.0.1a"
 
-from arviz_dashboard.marginal.one_d import posterior_marginal1d
 from .elpd import dashboard_elpd
 from .ppc import dashboard_ppc
+from arviz_dashboard.marginal.one_d import posterior_marginal1d
+from arviz_dashboard.trace.trace import trace
