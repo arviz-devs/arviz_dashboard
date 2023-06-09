a042a52ef78e1e5586f4535b7791f7bd83192601
YilinXia <xia.yl@outlook.com>
2023-06-09 16:42:10 -0500

    comments addressed

--- arviz_dashboard/forest.py
+++ arviz_dashboard/forest.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import arviz as az
 import bokeh.io
 import panel as pn
@@ -14,57 +15,61 @@ class ModelVar(param.Parameterized):
     model = param.Selector("")
     data_variable = param.Selector("")
     coor_variable = param.Selector("")
+    index_key = ""
 
-    def __init__(self, idatas_cmp, **params) -> None:
-        self.idatas_cmp = idatas_cmp
-        self.default_model = list(self.idatas_cmp.keys())[0]
-        self.param["model"].objects = list(self.idatas_cmp.keys())
+    def __init__(
+        self, idata_dict: dict[str, az.InferenceData], **params
+    ) -> None:
+        self.idata_dict = idata_dict
+        self.models = list(self.idata_dict.keys())
+        self.default_model = self.models[0]
+        self.param["model"].objects = self.models
         self.param["model"].default = self.default_model
         self.param["data_variable"].objects = list(
-            self.idatas_cmp[self.default_model].posterior.data_vars.variables
+            self.idata_dict[self.default_model].posterior.data_vars.variables
         )
         super().__init__(**params)
 
     @param.depends("model", watch=True)
-    def _update_data_variables(self):
+    def _update_data_variables(self: ModelVar):
         data_variables = list(
-            self.idatas_cmp[self.model].posterior.data_vars.variables
+            self.idata_dict[self.model].posterior.data_vars.variables
         )
         self.param["data_variable"].objects = data_variables
         if self.data_variable not in data_variables:
-            self.data_variable = data_variables[0]
+            self.data_variable = data_variables[0] if data_variables else None
 
     @param.depends("data_variable", watch=True)
-    def _update_coordinates(self):
+    def _update_coordinates(self: ModelVar):
         if (
-            self.idatas_cmp[self.model]
-            .posterior.data_vars.variables[self.data_variable][0][0]
-            .size
-            > 1
-        ):
+            self.idata_dict[self.model]
+            .posterior.data_vars.variables[self.data_variable].ndim
+            > 2
+        ):  
+            self.index_key = list(dict(self.idata_dict[self.model].posterior
+                                       .dims).keys())[2]
             coor_variables = list(
-                self.idatas_cmp[self.model].posterior.indexes["school"]
+                self.idata_dict[self.model].posterior.indexes[self.index_key]
             )
         else:
             coor_variables = [""]
         self.param["coor_variable"].objects = coor_variables
-        if self.coor_variable not in coor_variables:
-            self.coor_variable = coor_variables[0]
 
 
 class ForestDashboard(ModelVar):
-    def __init__(self, idatas_cmp) -> None:
-        self.idatas_cmp = idatas_cmp
-        super().__init__(self.idatas_cmp)
+    def __init__(self, idata_dict) -> None:
+        self.idata_dict = idata_dict
+        super().__init__(self.idata_dict)
 
     def dashboard_forest(self):
         # define the widgets
-        multi_select = pn.widgets.MultiSelect(
+        model_ls = list(self.idata_dict.keys())
+        models_selection_widget = pn.widgets.MultiSelect(
             name="ModelSelect",
-            options=list(self.idatas_cmp.keys()),
-            value=["mA"],
+            options=model_ls,
+            value=[model_ls[0]],
         )
-        thre_slider = pn.widgets.FloatSlider(
+        hdi_slider = pn.widgets.FloatSlider(
             name="HDI Probability",
             start=0,
             end=1,
@@ -72,8 +77,9 @@ class ForestDashboard(ModelVar):
             value=0.7,
             width=200,
         )
-        truncate_checkbox = pn.widgets.Checkbox(name="Ridgeplot Truncate")
-        ridge_quant = pn.widgets.RangeSlider(
+        ridgeplot_truncate_checkbox = pn.widgets.Checkbox(
+            name="Ridgeplot Truncate")
+        ridgeplot_quantiles = pn.widgets.RangeSlider(
             name="Ridgeplot Quantiles",
             start=0,
             end=1,
@@ -81,7 +87,7 @@ class ForestDashboard(ModelVar):
             step=0.01,
             width=200,
         )
-        op_slider = pn.widgets.FloatSlider(
+        ridgeplot_overlap_slider = pn.widgets.FloatSlider(
             name="Ridgeplot Overlap",
             start=0,
             end=1,
@@ -90,7 +96,7 @@ class ForestDashboard(ModelVar):
             width=200,
         )
 
-        rope_slider = pn.widgets.RangeSlider(
+        forestplot_rope_slider = pn.widgets.RangeSlider(
             name="Rope Range",
             start=-10,
             end=10,
@@ -101,60 +107,64 @@ class ForestDashboard(ModelVar):
 
         # construct widget
         @pn.depends(
-            multi_select.param.value,
-            thre_slider.param.value,
-            rope_slider.param.value,
+            models_selection_widget.param.value,
+            hdi_slider.param.value,
+            forestplot_rope_slider.param.value,
             self.param.data_variable,
             self.param.coor_variable,
         )
         def get_forest_plot(
-            multi_select, thre_slider, rope_slider, 
-            data_variable, coor_variable
+            models_selection_widget: pn.widgets.MultiSelect,
+            hdi_slider: pn.widgets.FloatSlider,
+            forestplot_rope_slider: pn.widgets.RangeSlider, 
+            data_variable: param.Selector, 
+            coor_variable: param.Selector
         ):
             # generate graph
             data = []
-            for model_ in multi_select:
-                data.append(self.idatas_cmp[model_])
+            for model in models_selection_widget:
+                data.append(self.idata_dict[model])
             # add rope
             rope = {}
-            school = {}
-            school["school"] = coor_variable
-            school["rope"] = rope_slider
-            rope[data_variable] = [school]
+            # e.g. "school" is one dimension of the xarray.Variable
+            idata_dim_dict = {}
+            idata_dim_dict[self.index_key] = coor_variable
+            idata_dim_dict["rope"] = forestplot_rope_slider
+            rope[data_variable] = [idata_dim_dict]
             # print(rope)
             forest_plt = az.plot_forest(
                 data,
-                model_names=multi_select,
+                model_names=models_selection_widget,
                 rope=rope,
                 kind="forestplot",
-                hdi_prob=thre_slider,
+                hdi_prob=hdi_slider,
                 backend="bokeh",
                 figsize=(9, 9),
                 show=False,
                 combined=True,
                 colors="cycle",
             )
-            return forest_plt[0][0]
+            # forest_plt is a narray as well as forest_plt[0], 
+            # thus we used forest_plt[0][0] to get the figure
+            return forest_plt.base[0]
 
         @pn.depends(
-            multi_select.param.value,
-            thre_slider.param.value,
-            truncate_checkbox.param.value,
-            ridge_quant.param.value,
-            op_slider.param.value,
+            models_selection_widget.param.value,
+            hdi_slider.param.value,
+            ridgeplot_truncate_checkbox.param.value,
+            ridgeplot_quantiles.param.value,
+            ridgeplot_overlap_slider.param.value,
         )
         def get_ridge_plot(
-            multi_select,
-            thre_slider,
-            truncate_checkbox,
-            ridge_quant,
-            op_slider,
+            models_selection_widget: pn.widgets.MultiSelect,
+            hdi_slider: pn.widgets.FloatSlider,
+            ridgeplot_truncate_checkbox: pn.widgets.Checkbox,
+            ridgeplot_quantiles: pn.widgets.RangeSlider,
+            ridgeplot_overlap_slider: pn.widgets.FloatSlider,
         ):
             # calculate the ridgeplot_quantiles
-            temp_quant = list(ridge_quant)
-            quant_ls = temp_quant
-            quant_ls.sort()
-            avg_quant = sum(temp_quant) / 2
+            quant_ls = sorted(list(ridgeplot_quantiles))
+            avg_quant = sum(quant_ls) / 2
             if quant_ls[0] < 0.5 and quant_ls[1] > 0.5:
                 quant_ls.append(0.5)
                 quant_ls.sort()
@@ -164,24 +174,24 @@ class ForestDashboard(ModelVar):
 
             # generate graph
             data = []
-            for model_ in multi_select:
-                data.append(self.idatas_cmp[model_])
+            for model_ in models_selection_widget:
+                data.append(self.idata_dict[model_])
 
             ridge_plt = az.plot_forest(
                 data,
-                model_names=multi_select,
+                model_names=models_selection_widget,
                 kind="ridgeplot",
-                hdi_prob=thre_slider,
-                ridgeplot_truncate=truncate_checkbox,
+                hdi_prob=hdi_slider,
+                ridgeplot_truncate=ridgeplot_truncate_checkbox,
                 ridgeplot_quantiles=quant_ls,
-                ridgeplot_overlap=op_slider,
+                ridgeplot_overlap=ridgeplot_overlap_slider,
                 backend="bokeh",
                 figsize=(9, 9),
                 show=False,
                 combined=True,
                 colors="white",
             )
-            return ridge_plt[0][0]
+            return ridge_plt.base[0]
 
         plot_result_1 = pn.Column(
             pn.WidgetBox(
@@ -191,20 +201,20 @@ class ForestDashboard(ModelVar):
                     self.param.data_variable,
                     self.param.coor_variable,
                 ),
-                rope_slider,
+                forestplot_rope_slider,
             ),
             get_forest_plot,
         )
         plot_result_2 = pn.Column(
-            pn.Row(truncate_checkbox),
-            pn.Row(ridge_quant, op_slider),
+            pn.Row(ridgeplot_truncate_checkbox),
+            pn.Row(ridgeplot_quantiles, ridgeplot_overlap_slider),
             get_ridge_plot,
         )
-        # show up
+        # show up in jupyter env
         display(
             pn.Column(
-                pn.Row(multi_select),
-                thre_slider,
+                pn.Row(models_selection_widget),
+                hdi_slider,
                 # pn.Row(self.param),
                 pn.Tabs(
                     ("Forest_Plot", plot_result_1),
