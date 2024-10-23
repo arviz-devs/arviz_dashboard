56618ebc411d2de6160740a9184813b44ea84b8a
Andy Maloney <amaloney@mailbox.org>
2024-10-23 17:50:46 -0500

    Traceplot with multiple hierarchies

--- src/arviz_dashboard/py/traceplot.py
+++ src/arviz_dashboard/py/traceplot.py
@@ -2,19 +2,20 @@ from pathlib import Path
 
 import anywidget
 import traitlets
+from xarray.core.datatree import DataTree
 
 from arviz_dashboard.py.models import PosteriorModel
 
 
 class Traceplot(anywidget.AnyWidget, PosteriorModel):
     _esm = Path(__file__).parent.parent.resolve() / "static" / "traceplot.js"
-    posterior = traitlets.Dict().tag(sync=True)
-    hierarchy = traitlets.Dict().tag(sync=True)
-    num_chains = traitlets.Integer().tag(sync=True)
-    num_draws = traitlets.Integer().tag(sync=True)
+    data = traitlets.Dict().tag(sync=True)
+    # hierarchy = traitlets.Dict().tag(sync=True)
+    # num_chains = traitlets.Integer().tag(sync=True)
+    # num_draws = traitlets.Integer().tag(sync=True)
 
-    def __init__(self, idata) -> None:
+    def __init__(self, idata: DataTree) -> None:
         data = self.parse_posterior_data(idata=idata)
-        self.num_chains = len(idata["posterior"].coords["chain"])
-        self.num_draws = len(idata["posterior"].coords["draw"])
-        super().__init__(posterior=data["posterior"], hierarchy=data["hierarchy"])
+        # self.num_chains = len(idata["posterior"].coords["chain"])
+        # self.num_draws = len(idata["posterior"].coords["draw"])
+        super().__init__(data=data)
--- src/arviz_dashboard/ts/traceplot.ts
+++ src/arviz_dashboard/ts/traceplot.ts
@@ -4,45 +4,85 @@ import {density1d} from "fast-kde/src/density1d"
 import {linearRange} from "arvizjs/src/lib/stats/array"
 import {getNestedObject} from "./utils"
 
+function determine_num_grid_columns(dropdowns) {
+    let num_dimensions = 0
+    for (let data_variable in dropdowns) {
+        const dimensions = Object.keys(dropdowns[data_variable]).length
+        num_dimensions = Math.max(num_dimensions, dimensions)
+    }
+    const num_columns = num_dimensions + 1
+    return num_columns
+}
+
+function create_grid_template_string(num_columns) {
+    let grid_template = ""
+    for (let i = 0; i < num_columns; i++) {
+        const percentage = Math.floor(90 * (1 / num_columns))
+        grid_template += `${percentage}% `
+    }
+    return grid_template
+}
+
 function render({model, el}) {
-    const posterior = model.get("posterior")
-    const num_chains = model.get("num_chains")
-    const num_draws = model.get("num_draws")
-    const hierarchy = model.get("hierarchy")
-    window.posterior = posterior
-    window.hierarchy = hierarchy
+    const data = model.get("data")
+    window.data = data
+    const dropdowns = data.dropdowns
+    const posterior = data.posterior
+    const num_chains = data.num_chains
+    const num_draws = data.num_draws
+    const num_grid_columns = determine_num_grid_columns(dropdowns)
+    const grid_template_columns = create_grid_template_string(num_grid_columns)
 
-    // Create a div for all the posterior inputs.
-    const posterior_input_div = document.createElement("div")
-    posterior_input_div.style.width = "1000px"
-    posterior_input_div.style.height = "100px"
-    posterior_input_div.style.display = "flex"
-    posterior_input_div.style.flexDirection = "row"
-    posterior_input_div.style.gap = "10px"
+    // Create a div for the traceplot.
+    const traceplot_div = document.createElement("div")
+    traceplot_div.style.width = "100%"
+    traceplot_div.style.height = "600px"
 
+    // Create a div for all the select dropdowns.
+    const select_div = document.createElement("div")
+    select_div.id = "select-div"
+    select_div.style.width = "100%"
+    select_div.style.height = "75px"
+    select_div.style.display = "grid"
+    select_div.style.gridTemplateColumns = grid_template_columns
+    select_div.style.columnGap = "10px"
+
+    // Create a div for the data variables select.
     const data_variable_div = document.createElement("div")
     data_variable_div.id = "data-variable-div"
+    data_variable_div.style.display = "grid"
+    data_variable_div.style.gridTemplateRows = "60% 40%"
     data_variable_div.addEventListener("change", add_dimensions)
     const data_variable_title = document.createElement("p")
     data_variable_title.innerHTML = "<b>Data variable</b>"
     data_variable_div.appendChild(data_variable_title)
     const data_variable_select = document.createElement("select")
-    data_variable_select.name = "data variables"
     data_variable_select.id = "data-variables-select"
+    data_variable_select.name = "data variables"
     data_variable_select.addEventListener("change", update_plot)
-    for (let data_variable in hierarchy) {
+    for (let data_variable in dropdowns) {
         data_variable_select.add(new Option(data_variable))
     }
     data_variable_div.appendChild(data_variable_select)
-    posterior_input_div.appendChild(data_variable_div)
+    select_div.appendChild(data_variable_div)
+
+    // Create a div for the extra dimension(s) of the selected data variable.
+    const extra_dimensions_div = document.createElement("div")
+    extra_dimensions_div.id = "extra-dimensions-div"
+    extra_dimensions_div.style.gridColumn = `2 / span ${num_grid_columns - 1}`
+    select_div.appendChild(extra_dimensions_div)
+
+    // Append the selects to the element.
+    el.appendChild(select_div)
 
     // Create a div for the chains.
     const chains_div = document.createElement("div")
-    chains_div.style.width = "1000px"
+    chains_div.style.width = "100%"
     chains_div.style.height = "25px"
     chains_div.style.display = "flex"
     chains_div.style.flexDirection = "row"
     chains_div.style.alignItems = "center"
+    chains_div.style.marginTop = "10px"
     for (let chain_num of [...Array(num_chains).keys()]) {
         const chain_checkbox = document.createElement("input")
         chain_checkbox.type = "checkbox"
@@ -57,53 +97,67 @@ function render({model, el}) {
         chains_div.appendChild(chain_label)
     }
 
+    // Append the chains to the element.
+    el.appendChild(chains_div)
+
+    // Create a div for the figures.
     const plot_div = document.createElement("div")
-    plot_div.style.width = "1000px"
+    plot_div.style.width = "100%"
     plot_div.style.height = "600px"
     plot_div.style.display = "flex"
     plot_div.style.flexDirection = "row"
 
-    // Create a div for all elements of the widget.
-    const div = document.createElement("div")
-    div.id = "forestplot-div"
-    div.appendChild(posterior_input_div)
-    div.appendChild(chains_div)
-    div.appendChild(plot_div)
-
-    // Append the widget div to the notebook element.
-    el.appendChild(div)
+    // Append the plot to the element.
+    el.appendChild(plot_div)
 
     // Callbacks
     function add_dimensions() {
-        let extra_dimensions_div = document.getElementById("extra-dimensions-div") as HTMLDivElement
-        if (extra_dimensions_div === null) {
-            extra_dimensions_div = document.createElement("div")
-            extra_dimensions_div.id = "extra-dimensions-div"
-        } else if (extra_dimensions_div !== null) {
-            extra_dimensions_div.innerHTML = ""
-        }
-        const extra_dimensions_title = document.createElement("p")
-        extra_dimensions_title.id = "extra-dimensions-title"
+        extra_dimensions_div.innerHTML = ""
         const data_variable = document.getElementById("data-variables-select") as HTMLSelectElement
         if (data_variable !== null) {
             const data_variable_name = data_variable.value
-            const coordinates = hierarchy[data_variable_name]
-            for (let coordinate in coordinates) {
-                extra_dimensions_title.innerHTML = `<b>${coordinate}</b>`
-                const extra_dimensions_select = document.createElement("select")
-                extra_dimensions_select.addEventListener("change", update_plot)
-                extra_dimensions_select.id = `coordinate-${coordinate}`
-                const dimensions = hierarchy[data_variable_name][coordinate]
+            const coordinates = dropdowns[data_variable_name]
+            const coordinate_keys = Object.keys(coordinates)
+            const extra_dimension_div = document.createElement("div")
+            extra_dimension_div.style.height = "100%"
+            extra_dimension_div.style.display = "grid"
+            extra_dimension_div.style.gridTemplateRows = "60% 40%"
+            extra_dimension_div.style.gridTemplateColumns = create_grid_template_string(
+                num_grid_columns - 1,
+            )
+            extra_dimension_div.style.columnGap = "10px"
+            for (let i = 0; i < coordinate_keys.length; i++) {
+                const coordinate_name = coordinate_keys[i]
+                extra_dimension_div.id = `coordinate-${coordinate_name}-div`
+                // Create a div for each additional dimension associated with the selected data
+                // variable.
+                extra_dimension_div.style.gridColumn = `${i + 1}`
+
+                // Title for the additional dimension.
+                const extra_dimension_title = document.createElement("p")
+                extra_dimension_title.id = `coordinate-${coordinate_name}-title`
+                extra_dimension_title.innerHTML = `<b>${coordinate_name}</b>`
+                extra_dimension_div.appendChild(extra_dimension_title)
+
+                // Create a select for all the additional dimensions.
+                const extra_dimension_select = document.createElement("select")
+                extra_dimension_select.id = `coordinate-${coordinate_name}-select`
+                extra_dimension_select.style.gridColumn = `${i + 1}`
+                extra_dimension_select.style.gridRow = "2"
+                extra_dimension_select.addEventListener("change", update_plot)
+
+                // Add the options for each additional dimension.
+                const dimensions = dropdowns[data_variable_name][coordinate_name]
                 if (Object.keys(dimensions).length !== 0) {
                     for (let dimension of dimensions) {
-                        extra_dimensions_select.add(new Option(dimension))
+                        extra_dimension_select.add(new Option(dimension))
                     }
                 }
-                extra_dimensions_div.appendChild(extra_dimensions_title)
-                extra_dimensions_div.appendChild(extra_dimensions_select)
+                extra_dimension_div.appendChild(extra_dimension_select)
+                extra_dimensions_div.appendChild(extra_dimension_div)
             }
         }
-        posterior_input_div.appendChild(extra_dimensions_div)
+        select_div.appendChild(extra_dimensions_div)
         update_plot()
     }
     add_dimensions()
@@ -117,20 +171,24 @@ function render({model, el}) {
         const data_variable_name = data_variable_select?.value
 
         // Find all dropdown elements associated with the chosen data variable.
-        const extra_dimensions = document.querySelectorAll('[id^="coordinate-"]')
+        const extra_dimensions = document.querySelectorAll('[id^="coordinate-"][id$=-select]')
+        const extra_dimensions_titles = document.querySelectorAll('[id^="coordinate-"][id$=-title]')
         // Cycle through all the dropdowns and get their values. We will use these
         // to find the data in the nested posterior data object.
         let extra_dimension_names = new Array()
+        let coordinate_names = new Array()
         for (let i = 0; i < extra_dimensions.length; i++) {
             const extra_dimension = extra_dimensions[i] as HTMLSelectElement
-            extra_dimension_names.push(extra_dimension?.value)
+            const extra_dimension_name = extra_dimension?.value
+            extra_dimension_names.push(extra_dimension_name)
+            const extra_dimension_title = extra_dimensions_titles[i] as HTMLElement
+            const coordinate = document.getElementById(
+                `coordinate-${extra_dimension_title.textContent}-title`,
+            )
+            const coordinate_name = coordinate?.textContent
+            coordinate_names.push(coordinate_name)
+            coordinate_names.push(extra_dimension_name)
         }
-
-        // We were able to get the extra dimension names, but we still need to get the
-        // coordinate name the extra dimension is associated with. This is found from
-        // title of the extra dimension dropdowns.
-        const coordinate = document.getElementById("extra-dimensions-title")
-        const coordinate_name = coordinate?.textContent
 
         // Set a few plot aesthetics for the traceplot.
         let opacity = 0.5
@@ -152,11 +210,11 @@ function render({model, el}) {
             let y = new Array()
             if (extra_dimensions.length !== 0) {
                 y = getNestedObject(
-                    posterior[data_variable_name]["coordinates"][coordinate_name],
-                    extra_dimension_names.concat(["chains", chain_num]),
+                    posterior,
+                    [data_variable_name].concat([...coordinate_names, "chain", chain_num]),
                 )
             } else {
-                y = posterior[data_variable_name]["chains"][chain_num]
+                y = posterior[data_variable_name]["chain"][chain_num]
             }
 
             // Plot the traceplot data.
